8dbc37eb1ca9ee5ca058251e01b24f727e0b6584
Bartłomiej Kulesa <bartekbpk@gmail.com>
2017-07-16 22:36:00 +0200

    Added MenuScreen and Player

--- core/src/com/bartekbpk/screens/MenuScreen.java
+++ core/src/com/bartekbpk/screens/MenuScreen.java
@@ -1,5 +1,6 @@
 package com.bartekbpk.screens;
 
+import com.bartekbpk.entities.Player;
 import com.bartekbpk.game.Arkanoid;
 
 /**
@@ -8,7 +9,34 @@ import com.bartekbpk.game.Arkanoid;
 
 public class MenuScreen extends AbstractScreen {
 
+    private Player player;
+
     public MenuScreen(Arkanoid game) {
         super(game);
+
+        initial();
+    }
+
+    private void initial() {
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+
     }
 }
